90e779e6e60d9c390971d4ff8cfc007958c27d0f
shinan6 <liushinan63@gmail.com>
2026-01-05 17:03:13 +0800

    Update news.js

--- data/news.js
+++ data/news.js
@@ -1,10 +1,28 @@
 // data/news.js
 const newsData = [
     { 
+        date: 'Jan 2026', 
+        title: 'Dr. Liu is invited to give a talk at the Harvard AI and Robotics Seminar!', 
+        content: 'Excited to share on "The Promise of Generative Synthetic Data for Networked Intelligent Systems", which highlights our latest works on generative AI for networking and systems.',
+        url: 'https://wang.hms.harvard.edu/news/professor-shinan-liu-seminar/' 
+    },
+    { 
+        date: 'Jan 2026', 
+        title: 'Dr. Liu serves as a TPC member for SIGCOMM, CCS, IMC, and PETS 2026.', 
+        content: 'Please consider submitting your papers to these venues!',
+        url: '' 
+    },
+    { 
         date: 'Dec 2025', 
         title: 'Our paper "WiFinger: Fingerprinting Noisy IoT Event Traffic Using Packet-level Sequence Matching" is accepted at NDSS 2026!', 
         content: 'A Non-ML technique for wireless fingerprinting of IoT event traffic under high noise, designed from first principles.',
         url: 'https://arxiv.org/pdf/2508.03151' 
+    },
+    { 
+        date: 'Dec 2025', 
+        title: 'NAISS Lab attended CoNEXT 2025 and Mobicom 2025, both located in Hong Kong!', 
+        content: 'We had a great time sharing our work and connecting with other researchers in the field.',
+        url: '' 
     },
     { 
         date: 'Sept 2025', 
