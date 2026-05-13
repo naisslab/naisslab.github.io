e7fe02e2bf621920b49a6a9c7e90bd57aa2b923e
shinan6 <liushinan63@gmail.com>
2026-05-13 17:54:36 +0800

    update Alumni section

--- data/people.js
+++ data/people.js
@@ -9,14 +9,17 @@ const peopleData = [
     { lastName: 'Guo', firstName: 'Zhaochen', role: 'Intern', education: 'Undergrad from UESTC', status: '2025 Summer - ', photo: 'images/people/zhaochen-guo.jpg' },
     { lastName: 'Li', firstName: 'Ronghua', role: 'Postdoc Scholar', education: 'Ph.D. from PolyU', status: '2026 Fall - ', photo: 'images/people/ronghua-li.jpg' },
     { lastName: 'Lian', firstName: 'Jinzhao', role: 'Intern', education: 'B.S. from RUC', status: '2026 Spring - ', photo: 'images/people/jinzhao-lian.jpg' },
-    { lastName: 'Qiu', firstName: 'Mingxi', role: 'Intern', education: 'M.S. from THU', status: '2025 Fall - Now', next: '-> Ph.D. Student at VT CS', photo: 'images/people/mingxi-qiu.jpg' },
     { lastName: 'Liu', firstName: 'Chengeng', role: 'Intern', education: 'M.S. from BU', status: '2026 Spring - ', photo: 'images/people/chengeng-liu.jpg' },
     { lastName: 'Luo', firstName: 'Zhifan', role: 'Ph.D. Student', education: 'M.S. from ZJU', status: '2026 Summer - ', photo: 'images/people/zhifan-luo.png', website: 'https://sio-2.github.io/' },
     { lastName: 'Sun', firstName: 'He', role: 'Postdoc Scholar', education: 'Ph.D. from USTC', status: '2026 Fall - ', photo: 'images/people/he-sun.jpg', website: 'https://ustc-sunny.github.io/' },
-    { lastName: 'Wang', firstName: 'Doris', role: 'Intern', education: 'M.S. from UToronto', status: '2025 Summer - 2026 Spring', next: '-> Ph.D. Student at NYU CS', photo: 'https://placehold.co/400x400/E8E2DD/4A4A4A?text=DW' },
-    { lastName: 'Yuan', firstName: 'Ziwen', role: 'Intern', education: 'M.S. from CMU', status: '2025 Fall - Now', next: '-> Ph.D. Student at PSU CS', photo: 'https://placehold.co/400x400/E8E2DD/4A4A4A?text=ZY' },
     { lastName: 'Zhang', firstName: 'Jialin', role: 'Intern', education: 'Undergrad from Tongji U', status: '2025 Summer - ', photo: 'images/people/jialin-zhang.jpg' },
     { lastName: 'Zhang', firstName: 'Jinyu', role: 'Intern', education: 'Undergrad from HKU', status: '2025 Summer - ', photo: 'images/people/jinyu-zhang.jpg' },
     { lastName: 'Zhang', firstName: 'Xinchen', role: 'Postdoc Scholar', education: 'Ph.D. from HKU', status: '2025 Fall - ', photo: 'images/people/xinchen-zhang.jpg', website: '#' },
     { lastName: 'Zhou', firstName: 'Tianyufei', role: 'MPhil Student', education: 'B.S. from SYSU', status: '2026 Spring - ', photo: 'images/people/tianyufei-zhou.jpg', website: 'https://ztyf.github.io/' }
+].sort((a, b) => a.lastName.localeCompare(b.lastName));
+
+const alumniData = [
+    { lastName: 'Qiu', firstName: 'Mingxi', role: 'Intern', education: 'M.S. from THU', status: '2025 Fall - 2026 Spring', next: '-> Ph.D. Student at VT CS', photo: 'images/people/mingxi-qiu.jpg' },
+    { lastName: 'Wang', firstName: 'Doris', role: 'Intern', education: 'M.S. from UToronto', status: '2025 Summer - 2026 Spring', next: '-> Ph.D. Student at NYU CS', photo: 'https://placehold.co/400x400/E8E2DD/4A4A4A?text=DW' },
+    { lastName: 'Yuan', firstName: 'Ziwen', role: 'Intern', education: 'M.S. from CMU', status: '2025 Fall - 2026 Spring', next: '-> Ph.D. Student at PSU CS', photo: 'https://placehold.co/400x400/E8E2DD/4A4A4A?text=ZY' }
 ].sort((a, b) => a.lastName.localeCompare(b.lastName));
